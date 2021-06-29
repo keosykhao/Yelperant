a9afd7561e28ad3f18112732065342ea6aea6fa2
Bouachanh Keosykhao <a_chanthavong@hotmail.com>
2021-06-29 19:00:52 -0400

    set up components

--- expense/client/src/App.jsx
+++ expense/client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowerRouter as Router, Switch, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Home from './routes/Home';
 import RestaurantPage from './routes/RestaurantPage';
 import Update from './routes/Update';
