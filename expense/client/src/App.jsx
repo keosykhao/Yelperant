5c9d7f168389a6f66cccdb7cb1d20a1630495ba9
Bouachanh Keosykhao <a_chanthavong@hotmail.com>
2021-06-29 18:54:03 -0400

    import react router dom

--- expense/client/src/App.jsx
+++ expense/client/src/App.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { BrowerRouter as Router, Switch, Route} from 'react-router-dom';
+import Home from './routes/Home';
+import RestaurantPage from './routes/RestaurantPage';
+import Update from './routes/Update';
 
 const App = () => {
     return <div>
-        App
+        <Router>
+            <Switch>
+                <Route exact path="/" component={Home}/>
+                <Route exact path="/restaurants/:id/update" component={Update}/>
+                <Route exact path="/restaurants/:id" component={RestaurantPage}/>
+
+            </Switch>
+        </Router>
+        
     </div>
 }
 
