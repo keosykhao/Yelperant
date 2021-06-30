2852e1e3856a02682dfc216829d5980ba642d0e5
Bouachanh Keosykhao <a_chanthavong@hotmail.com>
2021-06-29 23:19:50 -0400

    update state

--- expense/client/src/components/AddRestaurant.jsx
+++ expense/client/src/components/AddRestaurant.jsx
@@ -1,18 +1,42 @@
-import React from 'react'
+import React, {useState} from 'react'
+import RestaurantFinder from '../apis/RestaurantFinder'
 
 const AddRestaurant = () => {
+
+    const [name, setName] = useState("")
+    const [location, setLocation] = useState("")
+    const [priceRange, setPriceRange] = useState("Price Range")
+    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        try {
+            RestaurantFinder.post("/")
+        } catch (err) {
+            
+        }
+    }
+
     return (
         <div className="mb-4">
             <form action="">
                 <div className= "row">
                     <div className="col">
-                        <input type="text" className="form-control" placeholder="name" />
+                        <input value= {name} 
+                        onChange={e=> setName(e.target.value)} 
+                        type="text" className="form-control" 
+                        placeholder="name" />
                     </div>
                     <div className="col">
-                        <input type="text" className="form-control" placeholder="location"/>
+                        <input value= {location} 
+                        onChange={e => setLocation(e.target.value)} 
+                        type="text" className="form-control" 
+                        placeholder="location"/>
                     </div>
                     <div className="col">
-                        <select className="custom-select my-1 mr-sm-2">
+                        <select 
+                        value={priceRange}
+                        onChange={e => setPriceRange(e.target.value)}
+                        className="custom-select my-1 mr-sm-2">
                             <option disabled>Price Range</option>
                             <option value="1">$</option>
                             <option value="2">$$</option>
@@ -22,7 +46,10 @@ const AddRestaurant = () => {
                         </select>
                     </div>
                     <div className="col">
-                    <button className="btn btn-primary">Add</button>
+                    <button 
+                        onClick={handleSubmit}
+                        type="submit"
+                        className="btn btn-primary">Add</button>
                     </div>
                 </div>
             </form>
--- expense/client/src/components/RestaurantList.jsx
+++ expense/client/src/components/RestaurantList.jsx
@@ -32,15 +32,23 @@ const RestaurantList = (props) => {
                     </tr>
                 </thead>
                 <tbody>
-                    {restaurants.map(restaurant =>{
-                        <tr>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
+                    { restaurants && restaurants.map(restaurant =>{
+                        return(
+                            <tr key = {restaurant.id}>
+                            <td>restaurant.name</td>
+                            <td>restaurant.location</td>
+                            <td>{"$".repeat(restaurant.price_range)}</td>
+                            <td>reviews</td>
+                            <td>
+                                <button className="btn btn-warning">Update</button>
+                            </td>
+                            <td>
+                            <button className="btn btn-danger">Delete</button>
+                            </td>
                         </tr>
+
+                        )
+                       
                     })}
                     {/* <tr>
                         <td>Jollibees</td>
