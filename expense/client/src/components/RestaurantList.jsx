b78d506ab39ea69c9d0a842de57362cb50969c1f
Bouachanh Keosykhao <a_chanthavong@hotmail.com>
2021-06-30 08:36:10 -0400

    .

--- expense/client/src/components/RestaurantList.jsx
+++ expense/client/src/components/RestaurantList.jsx
@@ -15,7 +15,18 @@ const RestaurantList = (props) => {
         };
         fetchData();
         
-    },[])
+    },[]);
+
+    const handleDelete = async (id)=>{
+        try {
+        const response = await RestaurantFinder.delete(`/${id}`);
+        setRestaurants(restaurants.filter(restaurant => {
+            return restaurant.id !== id
+        }));
+        } catch (error) {
+            
+        }
+    };
 
 
     return (
@@ -43,7 +54,7 @@ const RestaurantList = (props) => {
                                 <button className="btn btn-warning">Update</button>
                             </td>
                             <td>
-                            <button className="btn btn-danger">Delete</button>
+                            <button onClick={()=>handleDelete(restaurant.id)} className="btn btn-danger">Delete</button>
                             </td>
                         </tr>
 
