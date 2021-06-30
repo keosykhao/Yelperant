06964f5fdccb3dc3b96aad94dbeb87429faca570
Bouachanh Keosykhao <a_chanthavong@hotmail.com>
2021-06-29 20:09:14 -0400

    set up forms

--- expense/client/src/routes/Home.jsx
+++ expense/client/src/routes/Home.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from 'react';
+import Header from '../components/Header';
+import AddRestaurant from '../components/AddRestaurant';
 
 const Home = () => {
     return (
         <div>
-            Home
+            <Header />
+            <AddRestaurant />
         </div>
     )
 }
